5959553f5510a2db53066910951d489fa6497a32
Bob Corsaro <bob@embed.ly>
2012-01-13 09:59:18 -0500

    Updates for batter api compat.

--- examples/oembed.php
+++ examples/oembed.php
@@ -62,25 +62,25 @@ jQuery(document).ready(function($) {
 </head>
   <body>
 <?php
+if (!getenv('EMBEDLY_KEY')) {
+    throw new \Exception('environmental variable EMBEDLY_KEY must be defined');
+}
 require_once "../src/Embedly/Embedly.php";
 $api = new Embedly\Embedly(array(
-  'user_agent' => 'Mozilla/5.0 (compatible; embedly/example-app; support@embed.ly)'
+    'user_agent' => 'Mozilla/5.0 (compatible; embedly/example-app; support@embed.ly)',
+    'key' => getenv('EMBEDLY_KEY')
 ));
 
 $urls = array(
-  "http://www.slideshare.net/doina/happy-easter-from-holland-slideshare", "http://www.scribd.com/doc/13994900/Easter",
-  "http://screenr.com/t9d",
-  "http://www.5min.com/Video/How-to-Decorate-Easter-Eggs-with-Decoupage-142076462",
+  "http://www.youtube.com/watch?v=iF3gzfel-a0",
   "http://www.howcast.com/videos/328008-How-To-Marble-Easter-Eggs",
   "http://img402.yfrog.com/i/mfe.jpg/",
-  "http://tweetphoto.com/14784359",
   "http://www.flickr.com/photos/10349896@N08/4490293418/",
   "http://imgur.com/6pLoN",
   "http://twitgoo.com/1as",
   "http://www.qwantz.com/index.php?comic=1686",
   "http://www.23hq.com/mhg/photo/5498347",
   "http://www.youtube.com/watch?v=gghKdx558Qg",
-  "http://www.justin.tv/easter7presents",
   "http://revision3.com/hak5/DualCore",
   "http://www.dailymotion.com/video/xcstzd_greek-wallets-tighten-during-easter_news",
   "http://www.collegehumor.com/video:1682246",
@@ -90,18 +90,19 @@ $urls = array(
   "http://www.metacafe.com/watch/105023/the_easter_bunny/",
   "http://blip.tv/file/449469",
   "http://video.google.com/videoplay?docid=-5427138374898988918&q=easter+bunny&pl=true",
-  "http://video.yahoo.com/watch/7268801/18963438",
   "http://www.viddler.com/explore/BigAppleChannel/videos/113/",
   "http://www.liveleak.com/view?i=e0b_1239827917",
   "http://www.hulu.com/watch/67313/howcast-how-to-make-braided-easter-bread",
-  "http://www.fancast.com/tv/It-s-the-Easter-Beagle,-Charlie-Brown/74789/1078053475/Peanuts:-Specials:-It-s-the-Easter-Beagle,-Charlie-Brown/videos",
   "http://www.funnyordie.com/videos/f6883f54ae/the-unsettling-ritualistic-origin-of-the-easter-bunny",
   "http://vimeo.com/10446922",
   "http://www.ted.com/talks/jared_diamond_on_why_societies_collapse.html",
   "http://www.thedailyshow.com/watch/thu-december-14-2000/intro---easter",
 );
 
-$oembeds = $api->oembed(array('urls' => $urls)); //, 'maxwidth' => 200));
+$oembeds = array();
+for ($i = 0; $i*20 < count($urls); $i++) {
+    $oembeds = array_merge($oembeds, $api->oembed(array('urls' => array_slice($urls, $i*20, 20))));
+}
 
 foreach ($oembeds as $k => $oembed) {
     $oembed = (array) $oembed;
@@ -143,13 +144,20 @@ foreach ($oembeds as $k => $oembed) {
     default:
     }
 
-    print '</div>';
-    print '<div class="description">';
-    if (array_key_exists('description', $oembed)) {
-        print $oembed['description'];
+    switch($oembed['type']) {
+    case 'error':
+        print '<p>Error</p>';
+        print '<div class="clear"></div>';
+        break;
+    default:
+        print '</div>';
+        print '<div class="description">';
+        if (array_key_exists('description', $oembed)) {
+            print $oembed['description'];
+        }
+        print '<div class="via">via <a href="'.$oembed['provider_url'].'">'.$oembed['provider_name'].'</a></div>';
+        print '</div>';
+        print '<div class="clear"></div>';
     }
-    print '<div class="via">via <a href="'.$oembed['provider_url'].'">'.$oembed['provider_name'].'</a></div>';
-    print '</div>';
-    print '<div class="clear"></div>';
 }
 ?></body></html>
